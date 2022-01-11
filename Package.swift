3bee6f01be2f9e5f4063d77d86594db6721f15f9
Venj Chu <ersaclarke@gmail.com>
2022-01-12 00:51:34 +0800

    Update package swift

--- Package.swift
+++ Package.swift
@@ -1,10 +1,11 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Lantern",
+    platforms: [.iOS(.v9)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -21,5 +22,6 @@ let package = Package(
         .target(
             name: "Lantern",
             dependencies: []),
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )
